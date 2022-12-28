81bdf991d9b2ec1d246d412c75a7d18ba7e2ac91
Eddie Rangel <hacker@eddierangel.com>
2022-12-28 01:11:41 -0800

    Create Payroll.cs
    Adding examples

---
+++ examples/Payroll.cs
@@ -0,0 +1,15 @@
+// Copyright hackingdotnet All Rights Reserved 
+
+using System;
+
+
+namespace Payroll
+{
+    class Prayroll
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello everyone, Welcome to My World!");
+        }
+    }
+}
